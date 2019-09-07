bb3188a349ee39df042203476ad65a2041febd94
Winston Zhang <winston.zhangwentong@gmail.com>
2019-09-08 01:09:55 +0800

    MOD:resultPath

--- wt_isign_macos.py
+++ wt_isign_macos.py
@@ -533,9 +533,10 @@ if __name__ == "__main__":
         else:
             # print("local")
             glt_source = os.path.join(os.path.dirname(__file__),source.split('/')[-1])
-
+            resultPath, _ = os.path.split(glt_source)
+            print('+++++++%s' % resultPath)
             if os.path.exists(source):
-                shutil.copytree(source,os.path.dirname(__file__))
+                shutil.copy(source,resultPath)
         print('拉取ipa完成：%s %s' % (ran_str,get_time()))
 
         print('获取sign_identity：%s %s' % (ran_str,get_time()))
