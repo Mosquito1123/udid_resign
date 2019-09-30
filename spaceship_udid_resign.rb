056fcb609337396dbc11a3eed8f4d1f186dfeb2a
Winston Zhang <winston.zhangwentong@gmail.com>
2019-09-30 11:49:37 +0800

    MOD:new

--- spaceship_udid_resign.rb
+++ spaceship_udid_resign.rb
@@ -110,7 +110,7 @@ default_keychain_result = default_keychain.strip
 `security unlock-keychain -p V@kP4eLnUU5l #{default_keychain_result}`
 user_name = options[:username]
 
-response = Spaceship::PortalClient.send_login_request(user_name,options[:password])
+response = Spaceship::PortalClient.new.send_login_request(user_name,options[:password])
 puts response
 spaceship = Spaceship::Launcher.new(user_name,options[:password])
 
